298d22436747782aeb2e318aaf0e4c2a98dd6bb9
沧海有泪 <127208503+1fantasy1@users.noreply.github.com>
2024-09-08 16:25:33 +0800

    Update theme.ts

--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -4,13 +4,13 @@ import sidebar from "./sidebar.js";
 
 export default hopeTheme({
   // 主题选项：https://theme-hope.vuejs.press/zh/config/theme/layout.html
-  hostname: "https://newzone.top",
+  hostname: "https://biji.675222.xyz",
   // 公用 CDN 在部分浏览器中可能无法正确显示图标。2024.07 开始，uBlock Origin 将拦截公有 CDN 的 js 请求。
   // https://icon-sets.iconify.design/
   iconAssets: "iconify",
   author: {
-    name: "LearnData",
-    url: "https://newzone.top",
+    name: "Fantasy",
+    url: "https://biji.675222.xyz",
   },
 
   favicon: "/favicon.ico",
@@ -22,7 +22,7 @@ export default hopeTheme({
   // copyright 默认为 Copyright © <作者>
   copyright: `
   版权声明：自由转载 - 非商用 - 非衍生 - 保持署名<a href="https://creativecommons.org/licenses/by-nc-nd/4.0/deed.zh-hans" target="_blank" rel="noopener noreferrer">（创意共享 4.0 许可证）</a>|
-  Copyright © 2023-present LearnData</a>`,
+  Copyright © 2023-present LearnData</a>`，
 
   displayFooter: true,
   // 页脚，支持使用 HTMLString 以显示备案信息等
@@ -42,7 +42,7 @@ export default hopeTheme({
   fullscreen: true,
 
   // 默认为 GitHub. 同时也可以是一个完整的 URL
-  repo: "rockbenben/LearnData",
+  repo: "1fantasy1/1fantasy1.github.io",
   // 自定义仓库链接文字。默认从 `repo` 中自动推断为 "GitHub" / "GitLab" / "Gitee" / "Bitbucket" 其中之一，或是 "Source"。
   repoLabel: "GitHub",
   // 是否在导航栏内显示仓库链接，默认为 `true`
@@ -77,13 +77,10 @@ export default hopeTheme({
     description: "迷信新工具，热衷于研究开源软件、心理学理论，定期分享探索成果",
     intro: "/intro.html",
     medias: {
-      GitHub: "https://github.com/rockbenben",
-      Zhihu: "https://www.zhihu.com/people/qingwhat",
-      少数派: { icon: "https://newzone.top/icons/sspai.svg", link: "https://sspai.com/u/zqj05i4v/posts" },
-      Wechat: "https://img.newzone.top/wechat.svg",
-      Email: "mailto:learndata@newzone.top",
-      Discord: "https://discord.gg/PZTQfJ4GjX",
-      RSS: "/rss.xml",
+      GitHub: "https://github.com/1fantasy1",
+      Zhihu: "https://www.zhihu.com/people/58-25-67-58-68",
+      少数派: { icon: "https://newzone.top/icons/sspai.svg", link: "https://sspai.com/u/fantasy_9928/posts" },
+      Email: "mail@675222.xyz",
     },
   },
 
