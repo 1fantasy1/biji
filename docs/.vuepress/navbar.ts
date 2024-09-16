83c5da7422211daebb07d06173f1a13c9b1506e9
fantasy <127208503+1fantasy1@users.noreply.github.com>
2024-09-16 23:59:22 +0800

    更新网站内容

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -1,9 +1,3 @@
-//导航栏配置
-import { navbar } from "vuepress-theme-hope";
-
-// 图标：https://theme-hope.vuejs.press/zh/guide/interface/icon.html#%E8%AE%BE%E7%BD%AE%E5%9B%BE%E6%A0%87
-// https://fontawesome.com/search?m=free&o=r
-// 专题话题的路径需在尾部添加 /，否则有可能出现链接错误。比如下方「生活」中的 baby/
 export default navbar([
   { text: "博客", icon: "fa6-solid:blog", link: "/blog" },
   {
@@ -54,5 +48,14 @@ export default navbar([
         link: "https://nav.newzone.top/",
       },
     ],
+  },
+  // 添加语言切换按钮
+  {
+    text: '简体中文',
+    link: '/zh/',
+  },
+  {
+    text: 'English',
+    link: '/',
   },
 ]);
