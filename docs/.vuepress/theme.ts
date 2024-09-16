0266941b949d8fe19ab411c8cca342db99061417
fantasy <127208503+1fantasy1@users.noreply.github.com>
2024-09-17 00:11:39 +0800

    更新网站内容

--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -2,6 +2,7 @@
 import { hopeTheme } from "vuepress-theme-hope";
 import navbar from "./navbar.js";
 import sidebar from "./sidebar.js";
+import navbar from './navbar';
 
 export default hopeTheme({
   // 主题选项：https://theme-hope.vuejs.press/zh/config/theme/layout.html
